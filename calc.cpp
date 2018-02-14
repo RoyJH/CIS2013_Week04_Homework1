b4984a1c22840547390bba7ce7aea8f430bf46c1
RoyJH <royjh@okstate.edu>
2018-02-14 17:41:55 -0600

    first commit
    Added a function, still need to add Modular and Sqrt.

---
+++ calc.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+void dothis (char& func)
+char func = '+';
+int main () 
+{
+	int first = 0;
+	int second = 0;
+	bool repeat = true;
+	char dothis = 't';
+
+	while (repeat = true){
+
+		cout << "What is the first number that you would like to calculate?: \n";
+		cin >> first;
+		cout <<  "What is the second number that you'd like to calculate?: \n";
+		cin >> second;
+		cout << " What type of operation should be performed on these numbers? +, -, *, or / :\n";
+		cin >> func;
+		dothis (char& func);
+
+	}
+	return 0;
+}
+void dothis (char& func)
+{
+		if (dothis == '+') {
+			cout << "The sum of your numbers is : " << first + second << endl;
+		}else if (dothis == '-') {
+			cout << "The difference of your numbers is : " << first - second << endl;
+		}else if (dothis == '*') {
+			cout << "The product of your numbers is: " << first * second << endl;
+		}else if (dothis == '/') {
+			cout << "The quotient of your numbers is: " << first / second << endl;
+		}		
+		cout << endl;
+}
